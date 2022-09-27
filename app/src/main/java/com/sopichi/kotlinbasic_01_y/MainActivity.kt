3767ed11885913c0248f68c0ab4800feb624a8d8
Y <yny.y147@gmail.com>
2022-09-27 23:35:25 +0900

    토스트 띄어보기

--- app/src/main/java/com/sopichi/kotlinbasic_01_y/MainActivity.kt
+++ app/src/main/java/com/sopichi/kotlinbasic_01_y/MainActivity.kt
@@ -3,6 +3,7 @@ package com.sopichi.kotlinbasic_01_y
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,13 +11,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        clickBtn.setOnClickListener {
-
-            // 코드에 메모 남기기 - 주석
+        // 코드에 메모 남기기 - 주석
 //            ctrl + / 커서가 있는 줄을 주석 처리
 
-     Log.d("메인화면", "클릭용 버튼 눌림")
-     Log.e("메인화면", "에러관련 로그")
+        clickBtn.setOnClickListener {
+
+            Log.d("메인화면", "클릭용 버튼 눌림")
+            Log.e("메인화면", "에러관련 로그")
+
+        }
+
+        smallBtn.setOnClickListener {
+
+            Toast.makeText(this, "작은 버튼 눌림", Toast.LENGTH_SHORT).show()
+
         }
     }
 }
