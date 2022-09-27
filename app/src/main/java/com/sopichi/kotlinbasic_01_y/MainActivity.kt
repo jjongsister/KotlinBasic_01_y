0a52fe88b2782eea6f57fc1f776cf7e7eefc153a
Y <yny.y147@gmail.com>
2022-09-27 21:49:33 +0900

    버튼 2개 추가 -> 기초 이벤트에 사용할 예정

--- app/src/main/java/com/sopichi/kotlinbasic_01_y/MainActivity.kt
+++ app/src/main/java/com/sopichi/kotlinbasic_01_y/MainActivity.kt
@@ -2,10 +2,15 @@ package com.sopichi.kotlinbasic_01_y
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        clickBtn
+        smallBtn
+
     }
 }
