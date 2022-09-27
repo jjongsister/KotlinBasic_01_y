836032061f1be6ec30f3d571fe2420e1105c606b
Y <yny.y147@gmail.com>
2022-09-27 23:22:18 +0900

    로그 찍어보기

--- app/src/main/java/com/sopichi/kotlinbasic_01_y/MainActivity.kt
+++ app/src/main/java/com/sopichi/kotlinbasic_01_y/MainActivity.kt
@@ -2,6 +2,7 @@ package com.sopichi.kotlinbasic_01_y
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,8 +10,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        clickBtn
-        smallBtn
+        clickBtn.setOnClickListener {
 
+            // 코드에 메모 남기기 - 주석
+//            ctrl + / 커서가 있는 줄을 주석 처리
+
+     Log.d("메인화면", "클릭용 버튼 눌림")
+        }
     }
 }
